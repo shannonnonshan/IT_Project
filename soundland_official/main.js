9d09db8e4d2bd055af034ef8b8c675cf170bc59c
shannonnonshan <khanhminhdoan04@gmail.com>
2024-11-02 20:41:22 +0700

    IT Project frame

---
+++ soundland_official/main.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import {dirname} from 'path';
+import {fileURLToPath} from 'url';
+import { engine } from 'express-handlebars';
+
+const _dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+app.use(express.urlencoded({
+    extended:true
+}));
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+
+app.listen(3000,function()
+{
+    console.log('ecApp us running at http://localhost:3000');
+});
