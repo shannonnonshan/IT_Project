0c380bd8ec96cbec077d086dd411917ec9f2b328
shannonnonshan <khanhminhdoan04@gmail.com>
2024-11-19 11:03:29 +0700

    User Page

--- application/music-player/js/constants.js
+++
@@ -1,8 +0,0 @@
-const API_KEY = "AIzaSyA5OWRCcjgM4tDV8Ar7n9J5z6ntTnRzkJw";
-
-const NOT_FOUND_SONG = "The song is not found";
-
-const VIDEO_YOUTUBE_ID = "video_youtube_id";
-const SELECTED_VIDEO_ID = "id-selected";
-
-const NOT_FOUND = "not found";
