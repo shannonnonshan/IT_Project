7105f6988616d22fee7767901f1c674f95cfd0de
“thanhvyhere” <dinhthithanhv.204@gmail.com>
2024-11-26 20:52:16 +0700

    link

--- soundland_official/main.js
+++ soundland_official/main.js
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 import { engine } from 'express-handlebars';
 import hbs_sections from 'express-handlebars-sections';
 import accountRouter from './routes/account.route.js'
+import homepageRouter from './routes/homepage.route.js'
 const __dirname = dirname(fileURLToPath(import.meta.url)); // Sử dụng __dirname với ES module
 
 const app = express();
@@ -28,14 +29,11 @@ app.set('view engine', 'hbs');
 app.set('views', './views');
 app.use('/static', express.static('static'));
 
-app.get('/', function(req,res)
-{
-// res.send('hello world');
-    res.render('home');
-});
+
 app.use('/css', express.static(path.join(__dirname, 'views', 'css')));
 app.use('/images', express.static(path.join(__dirname, 'views', 'images')));
 
+app.use('/home', homepageRouter);
 // Route Account:
 app.use('/account', accountRouter);
 // Khởi động server
