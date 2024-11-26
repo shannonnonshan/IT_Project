e630b5142e532e842bccf9b1db76c3fc2088f60a
22110077 <22110077@student.hcmute.edu.vn>
2024-11-26 13:55:05 +0700

    Thêm đường link vào mỗi album

---
+++ soundland_official/service/album.service.js
@@ -0,0 +1,23 @@
+export default
+{
+    AlbumList()
+    {
+        return
+        [
+            { id: 1, title: 'Disease', artist: 'Rosé, Bruno Mars', dateAdded: '6 days ago', duration: '3:50', cover: './images/apt nền.png' },
+            { id: 2, title: 'Buriki no Dance +DEVILA REMIX', artist: 'AnotherNicoUploader', dateAdded: '6 years ago', duration: '3:22', cover: './images/Buriki_no_Dance _+DEVILA_REMIX.jpg' },
+            { id: 3, title: 'Bad Never Looked So Good', artist: 'PattyCake Productions', dateAdded: '5 years ago', duration: '4:33', cover: './images/Bad_Never_Looked_So_Good.jpg' },
+            { id: 4, title: 'Alien Alien', artist: 'AnotherNicoUploader', dateAdded: '8 years ago', duration: '3:02', cover: './images/Alien_Alien.jpg' },
+            { id: 5, title: '클락션 (Klaxon)', artist: '(G)-IDLE', dateAdded: '3 months ago', duration: '3:06', cover: './images/클락션_(Klaxon).jpg' },
+            { id: 6, title: 'Outer Science', artist: 'AnotherNicoUploader', dateAdded: '10 years ago', duration: '3:36', cover: './images/Outer_Science.jpg' },
+            { id: 7, title: 'Blinding Lights', artist: 'The Weeknd', dateAdded: '2 years ago', duration: '3:20', cover: './images/Blinding_Lights.jpg' },
+            { id: 8, title: 'Levitating', artist: 'Dua Lipa ft. DaBaby', dateAdded: '1 year ago', duration: '3:23', cover: './images/Levitating.jpg' },
+            { id: 9, title: 'Good 4 U', artist: 'Olivia Rodrigo', dateAdded: '1 year ago', duration: '2:58', cover: './images/Good_4_U.jpg' },
+            { id: 10, title: 'Peaches', artist: 'Justin Bieber ft. Daniel Caesar, Giveon', dateAdded: '2 years ago', duration: '3:18', cover: './images/Peaches.jpg' },
+            { id: 11, title: 'Watermelon Sugar', artist: 'Harry Styles', dateAdded: '2 years ago', duration: '2:54', cover: './images/Watermelon_Sugar.jpg' },
+            { id: 12, title: 'Save Your Tears', artist: 'The Weeknd', dateAdded: '2 years ago', duration: '3:35', cover: './images/Save_Your_Tears.jpg' },
+            { id: 13, title: 'Stay', artist: 'Kid LAROI, Justin Bieber', dateAdded: '1 year ago', duration: '2:21', cover: './images/Stay.jpg' },
+            { id: 14, title: 'As It Was', artist: 'Harry Styles', dateAdded: '6 months ago', duration: '2:47', cover: './images/As_It_Was.jpg' }
+        ];
+    }
+}
