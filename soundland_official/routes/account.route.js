17b7895e654ecb4b153beea653e08e65405c5e30
shannonnonshan <khanhminhdoan04@gmail.com>
2024-12-04 21:54:52 +0700

    file audio/cover song

--- soundland_official/routes/account.route.js
+++ soundland_official/routes/account.route.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import bcrypt from 'bcryptjs';
 import moment from 'moment'; // format month day
+import multer from 'multer';
+import  { v4 as uuidv }  from 'uuid';
 import accountService from '../services/account.service.js';
 import userProfileService from '../services/userProfile.service.js';
 
 const router = express.Router();
+
 router.get('/signin', function (req, res) {
     res.render('vwAccount/sign-in', {
         layout: 'sign-up-layout'  // Sử dụng layout signUpLayout cho trang đăng ký
@@ -35,8 +38,41 @@ router.get('/profile', async function(req, res){
 });
 router.get('/upload', async function(req, res){
         res.render('vwAccount/uploadSong');
-        
-    });
+});
+
+// Cấu hình multer với bộ lọc file
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'static/songs');
+        },
+    filename: (req, file, cb) => {
+        const uniqueName = uuidv(); // UUID ngẫu nhiên, không có phần mở rộng
+        cb(null, uniqueName); // Lưu tên file
+    },
+  });
+  
+  // Kiểm tra định dạng file
+  const fileFilter = (req, file, cb) => {
+    const allowedTypes = ['audio/mpeg', 'audio/mp3', 'audio/wav', 'audio/x-wav', 'audio/ogg'];
+    if (allowedTypes.includes(file.mimetype)) {
+      cb(null, true); // Đồng ý upload
+    } else {
+      cb(new Error('Only audio files are allowed!'), false); // Từ chối file
+    }
+  };
+  
+  // Khởi tạo multer với bộ lọc
+  const upload = multer({
+    storage,
+    fileFilter, // Thêm fileFilter vào cấu hình
+  });
+router.post('/upload',upload.single('FilePath'), async function(req, res){
+    if (!req.file) {
+        return res.status(400).send('No file uploaded.');
+    }
+    const fileUrl = `/static/songs/${req.file.filename}`; 
+    res.render('vwAccount/uploadSong', { fileUrl });
+});
     
 router.post('/signup', async function (req, res) {
     const hash_password = bcrypt.hashSync(req.body.raw_password, 8);
