8f5428e7ff22077c8c2f9b6232a69e5cfab32185
“thanhvyhere” <dinhthithanhv.204@gmail.com>
2024-12-07 19:28:52 +0700

    get music

--- soundland_official/services/music.service.js
+++ soundland_official/services/music.service.js
@@ -1,21 +1,19 @@
 import db from '../utils/db.js'
 
 export default {
+    findAll() {
+        return db('songs');
+    },
     findSongById(id){
-        return db('songs').where('SongId', id).first();
+        return db('songs').where('SongID', id).first();
     },
     async findArtistBySongId(songId) {
-        // Lấy ArtistId từ bảng SongArtist
-        const songArtist = await db('songs').where('SongId', songId).first();
+    const songArtist = await db('song_artists').where('SongID', songId).first();
+    if (!songArtist) {
+        return null;
+    }
+    const artist = await db('artists').where('ArtistID', songArtist.ArtistID).first();
+    return artist;
+}
 
-        // Nếu không có liên kết, trả về null
-        if (!songArtist) {
-            return null;
-        }
-
-        // Lấy thông tin nghệ sĩ từ bảng Artists
-        const artist = await db('artists').where('ArtistId', songArtist.ArtistId).first();
-
-        return artist;
-    },
 };
